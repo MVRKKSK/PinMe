2c5c2e8a3be671830c41f1ebc37edbb3cda1eed9
MVRKKSK <miryala.kautilya2020@vitbhopal.ac.in>
2022-01-28 17:24:45 +0530

    Completed the backend work

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,22 +4,42 @@ const bcrypt = require("bcrypt")
 
 const router = express.Router();
 
-router.post("/register" , async(req , res)=>{
-    try{
+router.post("/register", async (req, res) => {
+    try {
         const salt = await bcrypt.genSalt(10);
         const hash = await bcrypt.hash(req.body.password, salt);
 
         const newUser = new User({
-            username : req.body.username,
-            email : req.body.email,
+            username: req.body.username,
+            email: req.body.email,
             password: hash
         })
 
         const savedUser = await newUser.save();
         res.status(200).json(savedUser);
     }
-    catch(err){
+    catch (err) {
         res.status(500).json(err);
+    }
+})
+
+router.post("/login", async (req, res) => {
+
+    try{
+        const findUser = await User.findOne({
+            username: req.body.username
+        });
+        !findUser && res.status(400).json("there is no user with that username")
+
+        const ValidPassword = await bcrypt.compare(
+            req.body.password,
+            findUser.password
+        )
+        !ValidPassword && res.status(400).json("incorrect password")
+        res.status(200).json({ _id: findUser._id, username: findUser.username })
+    }
+    catch(err){
+        res.status(400).json(err)
     }
 })
 
