2e07db1fa337d3158301924f2d344baae7aa6366
MVRKKSK <miryala.kautilya2020@vitbhopal.ac.in>
2022-02-19 13:09:30 +0530

    created a navbar

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,31 @@ import './App.css';
 import { Room } from '@material-ui/icons'
 import Login from './components/Login';
 import Register from './components/Register';
+import Navbar from './components/Navbar';
 function App() {
+
+  const [myLatitude, setMyLatitude] = useState()
+  const [myLongitude, setMyLongitude] = useState()
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setMyLatitude(position.coords.latitude)
+      setMyLongitude(position.coords.longitude)
+      console.log(myLatitude)
+      console.log(myLongitude)
+    })
+  } , [myLongitude , myLatitude])
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
-    latitude: 37.7577,
-    longitude: -122.4376,
+    latitude: myLatitude,
+    longitude: myLongitude,
     zoom: 8
   });
 
-  const currentusername = null;
-
+  const myStorage = window.localStorage;
+  const [currentUsername, setCurrentUsername] = useState(myStorage.getItem("user"));
+  const [implementSearch, setImplementSearch] = useState([])
 
 
   const [title, setTitle] = useState(null);
@@ -26,9 +40,14 @@ function App() {
   const [markerplace, setMarkerplace] = useState(null);
 
   const [pins, setPins] = useState([]);
-  const [dummy , setDummy] = useState(pins)
+  const [dummy, setDummy] = useState(pins)
 
   const [search, setSearch] = useState("")
+
+
+
+
+
 
   const handleClick = (id, lat, lon) => {
     setCurrentplaceid(id);
@@ -39,7 +58,7 @@ function App() {
   const handleonSubmit = async (e) => {
     e.preventDefault();
     const Addpin = {
-      username: currentusername,
+      username: currentUsername,
       title,
       description,
       rating,
@@ -67,8 +86,7 @@ function App() {
     })
   }
 
-  const myStorage = window.localStorage;
-  const [currentUsername, setCurrentUsername] = useState(myStorage.getItem("user"));
+
 
   const handleLogout = () => {
     setCurrentUsername(null);
@@ -88,34 +106,38 @@ function App() {
     getPins();
   }, [])
 
-   /*  const handleSearchChange = value => {
-      setSearch(value);
-      filterData(value);
-    };
-  
-    const filterData = (value) => {
-      const lowercasedValue = value.toLowerCase();
-      if (lowercasedValue === "") setPins(dummy);
-      else {
-        const filteredData = pins.filter(item => {
-          return Object.keys(item).some(key =>
-            item[key].toString().toLowerCase().includes(lowercasedValue)
-          );
-        });
-        setPins(filteredData);
-        console.log(pins)
-        console.log(filteredData)
-      }
-    } */
+  /*  const handleSearchChange = value => {
+     setSearch(value);
+     filterData(value);
+   };
+ 
+   const filterData = (value) => {
+     const lowercasedValue = value.toLowerCase();
+     if (lowercasedValue === "") setPins(dummy);
+     else {
+       const filteredData = pins.filter(item => {
+         return Object.keys(item).some(key =>
+           item[key].toString().toLowerCase().includes(lowercasedValue)
+         );
+       });
+       setPins(filteredData);
+       console.log(pins)
+       console.log(filteredData)
+     }
+   } */
 
-  const implementSearch = pins.filter(pin => { return (pin.title || pin.description).toLowerCase().includes(search.toLowerCase()) })
+  useEffect(() => {
+    setImplementSearch(
+      pins.filter(pin => { return (pin.title).toLowerCase().includes(search.toLowerCase()) })
+    )
+  }, [search, pins])
 
-  console.log(search)
 
 
 
   return (
     <div>
+      <Navbar />
       <ReactMapGL {...viewport}
         onDblClick={currentUsername && handleDoubleclick}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
@@ -124,7 +146,7 @@ function App() {
         {implementSearch.map((pin) => (
           <>
             <Marker latitude={pin.lat} longitude={pin.lon} offsetLeft={-20} offsetTop={-10}>
-              <Room style={{ fontSize: 30, color: "blue", cursor: "pointer", zIndex: -999 }}
+              <Room style={{ fontSize: 30, color: "blue", cursor: "pointer" }}
                 onClick={(id) => handleClick(pin._id, pin.lat, pin.lon)} />
             </Marker>
             {pin._id === currentplaceid && (
@@ -174,15 +196,15 @@ function App() {
             onClose={() => setMarkerplace(null)}
             anchor="top" >
             <form onSubmit={handleonSubmit}>
-              <div class="mb-6">
-                <label for="place" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Place</label>
-                <input onChange={(e) => { setTitle(e.target.value) }} type="text" id="place" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+              <div className="mb-6">
+                <label for="place" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Place</label>
+                <input onChange={(e) => { setTitle(e.target.value) }} type="text" id="place" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
               </div>
-              <div class="mb-6">
-                <label for="descripition" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Description</label>
-                <input onChange={(e) => { setDescription(e.target.value) }} type="text" id="description" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+              <div className="mb-6">
+                <label for="descripition" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Description</label>
+                <input onChange={(e) => { setDescription(e.target.value) }} type="text" id="description" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
               </div>
-              <select onChange={(e) => { setRating(e.target.value) }} class=" mb-6 form-select form-select-sm
+              <select onChange={(e) => { setRating(e.target.value) }} className=" mb-6 form-select form-select-sm
     appearance-none
     block
     w-full
@@ -205,21 +227,22 @@ function App() {
                 <option value="4">4</option>
                 <option value="5">5</option>
               </select>
-              <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add your place</button>
+              <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add your place</button>
             </form>
           </Popup>
         )}
-        {currentUsername ? (<div className='login-button-register'><button onClick={handleLogout} class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:ring-green-200 dark:focus:ring-green-800">
-          <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+        {currentUsername ? (<div className='login-button-register'><button onClick={handleLogout} className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:ring-green-200 dark:focus:ring-green-800">
+          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
             Logout
           </span>
-        </button>
-        </div>) : (<div className='login-button-register'><button onClick={() => setShowlogin(true)} class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:ring-pink-200 dark:focus:ring-pink-800">
-          <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+        </button><input className="mt-6 mb-6 " type="text" placeholder="search"
+          onChange={e => setSearch(e.target.value)} />
+        </div>) : (<div className='login-button-register'><button onClick={() => setShowlogin(true)} className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:ring-pink-200 dark:focus:ring-pink-800">
+          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
             Login
           </span></button>
-          <button onClick={() => setShowregister(true)} class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800">
-            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+          <button onClick={() => setShowregister(true)} className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800">
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
               Register
             </span>
           </button><input className="mt-6 mb-6 " type="text" placeholder="search"
